6e812da972760f3acb119d8de58eac25190c1d07
valeria mariscal <believeinthefuture@outlook.com>
2019-01-11 19:54:56 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <!-- v-bind directive passes todos(in data) to Todos.vue component -->
+    <!-- v-on catched the emited delete todo event -->
+    <Header />
+    <!-- catch the emit for the new todo -->
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+export default {
+  name: 'app',
+  components: {
+    // make sure to add all compponents in export
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Sometimes I like to diverge",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      // set this.todos to current todos.filter(arr => todo.id != id)
+      this.todos = this.todos.filter(todo => todo.id != id);
+    },
+    addTodo(newTodo) {
+      console.log('add todo');
+      this.todos = [...this.todos, newTodo];
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+    .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+<!-- v-bind adds the class 'is-completed' to a todo with complete: ture -->
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <!-- v-on is the directive for an event -->
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class='del'>X</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete(){
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+
+    .is-complete {
+    text-decoration: line-through;
+  }
+
+    .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
